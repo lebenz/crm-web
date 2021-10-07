f860f62b33b7c894910bc6c181276db663bccc65
lebenz <lebenz@163.com>
2021-10-07 23:34:03 +0800

    功能页面代码提交

---
+++ src/views/customer/customerList/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <p>customerList</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customerList'
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
